a23cdf25e235b3abb89dcf2f11804baf3bd5e08d
mehaotian <490272692@qq.com>
2020-03-29 22:56:11 +0800

    feat : 标签管理页面布局

--- components/tab/tab.vue
+++ components/tab/tab.vue
@@ -6,7 +6,7 @@
 				 @click="clickTab(item, index)">{{item.name}}</view>
 			</view>
 		</scroll-view>
-		<view class="tab-icons">
+		<view class="tab-icons" @click="open">
 			<uni-icons type="gear" size="26" color="#666"></uni-icons>
 		</view>
 	</view>
@@ -44,6 +44,11 @@
 				this.$emit('tab', {
 					data: item,
 					index: index
+				})
+			},
+			open(){
+				uni.navigateTo({
+					url:"/pages/home-label/home-label"
 				})
 			}
 		}
---
+++ pages/home-label/home-label.vue
@@ -0,0 +1,86 @@
+<template>
+	<view class="label">
+		<view class="label-box">
+			<view class="label-header">
+				<view class="label-title">我的标签</view>
+				<view class="label-edit">编辑</view>
+			</view>
+			<view class="label-content">
+				<view class="label-content__item" v-for="item in 10">
+					{{item}}标签
+					<uni-icons class="icons-close" type="clear" size="20" color="red"></uni-icons>
+				</view>
+			</view>
+			
+		</view>
+		<view class="label-box">
+			<view class="label-header">
+				<view class="label-title">标签推荐</view>
+			</view>
+			<view class="label-content">
+				<view class="label-content__item" v-for="item in 10">{{item}}标签</view>
+			</view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	page  {
+		background-color: #f5f5f5;
+	}
+	.label {
+		.label-box {
+			background-color: #fff;
+			margin-bottom: 10px;
+			.label-header {
+				display: flex;
+				justify-content: space-between;
+				padding: 10px  15px;
+				font-size: 14px;
+				color: #666;
+				border-bottom: 1px #f5f5f5 solid;
+				.label-edit {
+					color: #30b33a;
+					font-weight: bold;
+				}
+			}
+			.label-content {
+				display: flex;
+				flex-wrap: wrap;
+				padding: 15px;
+				padding-top: 0;
+				.label-content__item {
+					position: relative;
+					padding: 2px 5px;
+					margin-top: 12px;
+					margin-right: 10px;
+					border-radius: 5px;
+					border: 1px #666 solid;
+					font-size: 14px;
+					color: #666;
+					.icons-close {
+						position: absolute;
+						right: -8px;
+						top: -8px;
+						background-color: #fff;
+						border-radius: 50%;
+					}
+				}
+			}
+		}
+	}
+</style>
