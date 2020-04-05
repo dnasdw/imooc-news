50d59c6ff655d25eea43344451b1f56639f71d05
mehaotian <490272692@qq.com>
2020-04-05 17:21:46 +0800

    feat : 保存标签页数据

--- pages/home-label/home-label.vue
+++ pages/home-label/home-label.vue
@@ -6,12 +6,12 @@
 				<view class="label-edit" @click="editLabel">{{is_edit?'完成':'编辑'}}</view>
 			</view>
 			<view class="label-content">
-				<view class="label-content__item" v-for="(item,index) in labelList" :key="item._id" >
+				<view class="label-content__item" v-for="(item,index) in labelList" :key="item._id">
 					{{item.name}}
 					<uni-icons v-if="is_edit" class="icons-close" type="clear" size="20" color="red" @click="del(index)"></uni-icons>
 				</view>
 			</view>
-			
+
 		</view>
 		<view class="label-box">
 			<view class="label-header">
@@ -20,7 +20,7 @@
 			<view class="label-content">
 				<view class="label-content__item" v-for="(item,index) in list" :key="item._id" @click="add(index)">{{item.name}}</view>
 			</view>
-			
+
 		</view>
 	</view>
 </template>
@@ -29,35 +29,62 @@
 	export default {
 		data() {
 			return {
-				is_edit:false,
-				labelList:[],
-				list:[]
+				is_edit: false,
+				labelList: [],
+				list: []
 			}
 		},
 		onLoad() {
 			this.getLabel()
 		},
 		methods: {
-			editLabel(){
-				this.is_edit = !this.is_edit
+			editLabel() {
+				// this.is_edit = !this.is_edit
+				// true 正在编辑
+				if (this.is_edit) {
+					this.is_edit = false
+					this.setUpdateLabel(this.labelList)
+				} else {
+					this.is_edit = true
+				}
 			},
-			add(index){
-				if(!this.is_edit) return
+			add(index) {
+				if (!this.is_edit) return
 				this.labelList.push(this.list[index])
-				this.list.splice(index,1)
+				this.list.splice(index, 1)
 			},
-			del(index){
+			del(index) {
 				this.list.push(this.labelList[index])
-				this.labelList.splice(index,1)
+				this.labelList.splice(index, 1)
 			},
-			getLabel(){
-				this.$api.get_label({
-					type:'all'
-				}).then((res)=>{
+			setUpdateLabel(label) {
+				let newArrIds = []
+				label.forEach(item => {
+					newArrIds.push(item._id)
+				})
+				uni.showLoading()
+				console.log(newArrIds);
+				this.$api.update_label({
+					label: newArrIds
+				}).then((res) => {
+					uni.hideLoading()
+					uni.showToast({
+						title:'更新成功',
+						icon:'none'
+					})
 					console.log(res);
-					const {data} = res
-					this.labelList = data.filter(item=>item.current)
-					this.list = data.filter(item=>!item.current)
+				})
+			},
+			getLabel() {
+				this.$api.get_label({
+					type: 'all'
+				}).then((res) => {
+					console.log(res);
+					const {
+						data
+					} = res
+					this.labelList = data.filter(item => item.current)
+					this.list = data.filter(item => !item.current)
 				})
 			}
 		}
@@ -65,30 +92,35 @@
 </script>
 
 <style lang="scss">
-	page  {
+	page {
 		background-color: #f5f5f5;
 	}
+
 	.label {
 		.label-box {
 			background-color: #fff;
 			margin-bottom: 10px;
+
 			.label-header {
 				display: flex;
 				justify-content: space-between;
-				padding: 10px  15px;
+				padding: 10px 15px;
 				font-size: 14px;
 				color: #666;
 				border-bottom: 1px #f5f5f5 solid;
+
 				.label-edit {
 					color: #30b33a;
 					font-weight: bold;
 				}
 			}
+
 			.label-content {
 				display: flex;
 				flex-wrap: wrap;
 				padding: 15px;
 				padding-top: 0;
+
 				.label-content__item {
 					position: relative;
 					padding: 2px 5px;
@@ -98,6 +130,7 @@
 					border: 1px #666 solid;
 					font-size: 14px;
 					color: #666;
+
 					.icons-close {
 						position: absolute;
 						right: -8px;
