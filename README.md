397f8d7f402373905ef0ad100251ffff5d1a81d2
mehaotian <490272692@qq.com>
2020-04-06 22:03:36 +0800

    feat: 发布评论页面展示

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# 富文本渲染
-1. 使用 uParse 组件去解析渲染富文本 [参考](https://ext.dcloud.net.cn/plugin?id=364)
-2. 可以课下了解 rich-text 组件 [参考](https://uniapp.dcloud.io/component/rich-text)
+# 发布评论页面展示
+1. 使用 popup 组件去实现发布弹窗，各种属性 [参考](https://ext.dcloud.net.cn/plugin?id=329)
+2. 如果要去操作页面元素，如使用ref属性，或者获取节点信息，应该在 onReady 中去实现，而不是onLoad ，onLoad生命周期 实在页面初始化就会去执行，而 onRead 是在页面渲染完毕去执行的
--- pages/home-detail/home-detail.vue
+++ pages/home-detail/home-detail.vue
@@ -24,7 +24,7 @@
 			</view>
 		</view>
 		<view class="detail-bottom">
-			<view class="detail-bottom__input">
+			<view class="detail-bottom__input" @click="openComment">
 				<text>谈谈你的看法</text>
 				<uni-icons type="compose" size="16" color="#F07373"></uni-icons>
 			</view>
@@ -40,6 +40,18 @@
 				</view>
 			</view>
 		</view>
+		<uni-popup ref="popup" type="bottom" :maskClick="false">
+			<view class="popup-wrap">
+				<view class="popup-header">
+					<text class="popup-header__item" @click="close">取消 </text>
+					<text  class="popup-header__item" @click="sumbit">发布 </text>
+				</view>
+				<view class="popup-content">
+					<textarea class="popup-textarea" v-model="commentsValue" maxlength="200" fixed placeholder="请输入评论内容" />
+					<view class="popup-count">{{commentsValue.length}}/200</view>
+				</view>
+			</view>
+		</uni-popup>
 	</view>
 </template>
 
@@ -52,14 +64,31 @@
 		data() {
 			return {
 				formData: {},
-				noData:'<p style="text-align:center;color:#666">详情加载中...</p>'
+				noData:'<p style="text-align:center;color:#666">详情加载中...</p>',
+				// 输入框的值
+				commentsValue:''
 			}
 		},
 		onLoad(query) {
 			this.formData = JSON.parse(query.params)
 			this.getDetail()
 		},
+		onReady() {
+		},
 		methods: {
+			// 打开评论发布窗口
+			openComment(){
+				this.$refs.popup.open()
+			},
+			// 关闭弹窗
+			close(){
+				this.$refs.popup.close()
+			},
+			// 发布
+			sumbit(){
+				console.log('发布');
+				this.$refs.popup.close()
+			},
 			// 获取详情信息
 			getDetail() {
 				this.$api.get_detail({
@@ -180,4 +209,36 @@
 			}
 		}
 	}
+	.popup-wrap {
+		background-color: #fff;
+		.popup-header {
+			display: flex;
+			justify-content: space-between;
+			font-size: 14px;
+			color: #666;
+			border-bottom: 1px #F5F5F5 solid;
+			.popup-header__item {
+				height: 50px;
+				line-height: 50px;
+				padding: 0 15px;
+			}
+		}
+		.popup-content {
+			width: 100%;
+			padding: 15px;
+			box-sizing: border-box;
+			.popup-textarea {
+				width: 100%;
+				height: 200px;
+				
+			}
+			.popup-count {
+				display: flex;
+				justify-content: flex-end;
+				font-size: 12px;
+				color: #999;
+			}
+		}
+	}
+
 </style>
